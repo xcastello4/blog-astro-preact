fa19fed71d0a88957d654f53ab1fc1f45264e2c3
Xavier Castello <xavier.castello@seidor.es>
2023-06-13 17:08:43 +0200

    Islas Astro completed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Xavi | Blog',
     description: 'Mi viaje de aprendizaje de Astro',
-    site: 'https://my-blog-astro.netlify.app',
+    site: 'https://blog-astro-preact.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
